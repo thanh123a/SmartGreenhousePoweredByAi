83e1e6946ba2c5e4fe9bbb28084697c7b5f17d0d
Tuan Thanh <killpenta2804@gmail.com>
2025-01-02 15:48:24 +0700

    commmit code

---
+++ MoblieApplication/app/src/main/java/com/example/moblieapplication/chatbot/Gemini.java
@@ -0,0 +1,10 @@
+package com.example.moblieapplication.chatbot;
+
+
+public class Gemini {
+    final public static String GEMINI_API_KEY = "AIzaSyCNjZv7VPg1b_2myuSMtLQjel5HVGLLzmg";
+
+    public Gemini() {
+    }
+
+}
